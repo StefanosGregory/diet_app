a5d0144b6f1dd1b40db2c00a4704bfa230cb23b6
stefanosgregory <gregorystefanos@gmail.com>
2021-04-11 14:17:37 +0300

    Repair comments

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,13 @@
-/*
- * Στέφανος Γρηγόρη  Π18178
+/* Diet Application
+ * Στέφανος Γρηγόρη Π18178
  * Κωνσταντίνος Κωνσταντινίδης Π18180
  * Στυλιανή Καλογήρου Π18181
  */
-
 
 public class Main {
 
     // main method
     public static void main(String[] args){
-        //System.out.println("Hello World");
-
         new com.Classes.ReadCSV();
     }
 }
