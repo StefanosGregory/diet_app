8561fa46faa7828e5b9975d9dec55bc91e5a9b01
Bakeries <bakeries.g9@gmail.com>
2021-05-26 21:33:06 +0300

    small update.

--- DietApp/UCClients.Designer.cs
+++ DietApp/UCClients.Designer.cs
@@ -29,13 +29,18 @@ namespace DietApp
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle4 = new System.Windows.Forms.DataGridViewCellStyle();
             this.label1 = new System.Windows.Forms.Label();
-            this.showClients_pnl = new System.Windows.Forms.Panel();
             this.search_txt = new System.Windows.Forms.TextBox();
             this.searchType_cb = new System.Windows.Forms.ComboBox();
             this.clear_btn = new DietApp.RoundButtons();
             this.Search_btn = new DietApp.RoundButtons();
             this.AddClient_btn = new DietApp.RoundButtons();
+            this.showClients_pnl = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize) (this.showClients_pnl)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -50,33 +55,26 @@ namespace DietApp
             this.label1.TabIndex = 2;
             this.label1.Text = "Clients";
             // 
-            // showClients_pnl
-            // 
-            this.showClients_pnl.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (37)))), ((int) (((byte) (42)))), ((int) (((byte) (64)))));
-            this.showClients_pnl.Location = new System.Drawing.Point(33, 142);
-            this.showClients_pnl.Margin = new System.Windows.Forms.Padding(2);
-            this.showClients_pnl.Name = "showClients_pnl";
-            this.showClients_pnl.Size = new System.Drawing.Size(785, 497);
-            this.showClients_pnl.TabIndex = 3;
-            // 
             // search_txt
             // 
-            this.search_txt.Font = new System.Drawing.Font("Arial Narrow", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.search_txt.Location = new System.Drawing.Point(503, 101);
+            this.search_txt.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.search_txt.Location = new System.Drawing.Point(455, 101);
             this.search_txt.Name = "search_txt";
             this.search_txt.Size = new System.Drawing.Size(125, 23);
             this.search_txt.TabIndex = 9;
             // 
             // searchType_cb
             // 
+            this.searchType_cb.DisplayMember = "Name";
             this.searchType_cb.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.searchType_cb.Font = new System.Drawing.Font("Arial Narrow", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.searchType_cb.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.searchType_cb.FormattingEnabled = true;
             this.searchType_cb.Items.AddRange(new object[] {"Name", "Telephone", "Email"});
-            this.searchType_cb.Location = new System.Drawing.Point(423, 101);
+            this.searchType_cb.Location = new System.Drawing.Point(375, 101);
             this.searchType_cb.Name = "searchType_cb";
-            this.searchType_cb.Size = new System.Drawing.Size(74, 23);
+            this.searchType_cb.Size = new System.Drawing.Size(74, 21);
             this.searchType_cb.TabIndex = 10;
+            this.searchType_cb.ValueMember = "Name";
             // 
             // clear_btn
             // 
@@ -86,8 +84,8 @@ namespace DietApp
             this.clear_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
             this.clear_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
             this.clear_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.clear_btn.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
-            this.clear_btn.Location = new System.Drawing.Point(729, 101);
+            this.clear_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.clear_btn.Location = new System.Drawing.Point(681, 101);
             this.clear_btn.Name = "clear_btn";
             this.clear_btn.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int) (((byte) (124)))), ((int) (((byte) (10)))), ((int) (((byte) (2)))));
             this.clear_btn.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int) (((byte) (124)))), ((int) (((byte) (10)))), ((int) (((byte) (2)))));
@@ -106,9 +104,9 @@ namespace DietApp
             this.Search_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
             this.Search_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
             this.Search_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Search_btn.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.Search_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.Search_btn.ForeColor = System.Drawing.Color.White;
-            this.Search_btn.Location = new System.Drawing.Point(634, 101);
+            this.Search_btn.Location = new System.Drawing.Point(586, 101);
             this.Search_btn.Name = "Search_btn";
             this.Search_btn.OnHoverBorderColor = System.Drawing.Color.FromArgb(((int) (((byte) (101)))), ((int) (((byte) (89)))), ((int) (((byte) (103)))));
             this.Search_btn.OnHoverButtonColor = System.Drawing.Color.FromArgb(((int) (((byte) (101)))), ((int) (((byte) (89)))), ((int) (((byte) (103)))));
@@ -129,7 +127,7 @@ namespace DietApp
             this.AddClient_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
             this.AddClient_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
             this.AddClient_btn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.AddClient_btn.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.AddClient_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
             this.AddClient_btn.ForeColor = System.Drawing.Color.White;
             this.AddClient_btn.Location = new System.Drawing.Point(33, 101);
             this.AddClient_btn.Name = "AddClient_btn";
@@ -143,28 +141,85 @@ namespace DietApp
             this.AddClient_btn.UseVisualStyleBackColor = true;
             this.AddClient_btn.Click += new System.EventHandler(this.AddClient_btn_Click);
             // 
+            // showClients_pnl
+            // 
+            this.showClients_pnl.AllowUserToAddRows = false;
+            this.showClients_pnl.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (126)))), ((int) (((byte) (249)))));
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (126)))), ((int) (((byte) (249)))));
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.showClients_pnl.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.showClients_pnl.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.showClients_pnl.BackgroundColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
+            this.showClients_pnl.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.showClients_pnl.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.showClients_pnl.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (128)))), ((int) (((byte) (255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (15)))), ((int) (((byte) (82)))), ((int) (((byte) (186)))));
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (15)))), ((int) (((byte) (82)))), ((int) (((byte) (186)))));
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (128)))), ((int) (((byte) (255)))));
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.showClients_pnl.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.showClients_pnl.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Transparent;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (126)))), ((int) (((byte) (249)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.showClients_pnl.DefaultCellStyle = dataGridViewCellStyle3;
+            this.showClients_pnl.EnableHeadersVisualStyles = false;
+            this.showClients_pnl.GridColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
+            this.showClients_pnl.Location = new System.Drawing.Point(33, 156);
+            this.showClients_pnl.Name = "showClients_pnl";
+            this.showClients_pnl.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Single;
+            dataGridViewCellStyle4.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle4.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            dataGridViewCellStyle4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            dataGridViewCellStyle4.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (126)))), ((int) (((byte) (249)))));
+            dataGridViewCellStyle4.SelectionBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (126)))), ((int) (((byte) (249)))));
+            dataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            dataGridViewCellStyle4.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.showClients_pnl.RowHeadersDefaultCellStyle = dataGridViewCellStyle4;
+            this.showClients_pnl.RowTemplate.DefaultCellStyle.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            this.showClients_pnl.RowTemplate.DefaultCellStyle.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            this.showClients_pnl.RowTemplate.DefaultCellStyle.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte) (0)));
+            this.showClients_pnl.RowTemplate.DefaultCellStyle.ForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (126)))), ((int) (((byte) (249)))));
+            this.showClients_pnl.RowTemplate.DefaultCellStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int) (((byte) (0)))), ((int) (((byte) (126)))), ((int) (((byte) (249)))));
+            this.showClients_pnl.RowTemplate.DefaultCellStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int) (((byte) (20)))), ((int) (((byte) (30)))), ((int) (((byte) (54)))));
+            this.showClients_pnl.RowTemplate.DefaultCellStyle.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.showClients_pnl.Size = new System.Drawing.Size(898, 453);
+            this.showClients_pnl.TabIndex = 11;
+            this.showClients_pnl.CellValueChanged += new System.Windows.Forms.DataGridViewCellEventHandler(this.showClients_pnl_CellValueChanged);
+            // 
             // UcClients
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int) (((byte) (46)))), ((int) (((byte) (51)))), ((int) (((byte) (73)))));
+            this.Controls.Add(this.showClients_pnl);
             this.Controls.Add(this.searchType_cb);
             this.Controls.Add(this.search_txt);
             this.Controls.Add(this.clear_btn);
             this.Controls.Add(this.Search_btn);
             this.Controls.Add(this.AddClient_btn);
-            this.Controls.Add(this.showClients_pnl);
             this.Controls.Add(this.label1);
             this.ForeColor = System.Drawing.Color.Transparent;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "UcClients";
             this.Size = new System.Drawing.Size(1067, 665);
             this.Load += new System.EventHandler(this.UcClients_Load);
+            ((System.ComponentModel.ISupportInitialize) (this.showClients_pnl)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
         }
-
-        private System.Windows.Forms.Panel showClients_pnl;
 
         private System.Windows.Forms.Button EditClient_btn;
 
@@ -176,8 +231,10 @@ namespace DietApp
         private System.Windows.Forms.Button button2;
         private RoundButtons AddClient_btn;
         private DietApp.RoundButtons Search_btn;
-        private RoundButtons clear_btn;
+        private DietApp.RoundButtons clear_btn;
         private System.Windows.Forms.TextBox search_txt;
         private System.Windows.Forms.ComboBox searchType_cb;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView showClients_pnl;
     }
 }
--- DietApp/UCClients.cs
+++ DietApp/UCClients.cs
@@ -1,11 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using Npgsql;
 using NpgsqlTypes;
@@ -15,9 +10,25 @@ namespace DietApp
     public partial class UcClients : UserControl
     {
         private const string Cs = "Host=localhost; Username=diet; Password=dietapp2021; Database=dietdb";
+        private readonly NpgsqlConnection _conn;
         public UcClients()
         {
             InitializeComponent();
+
+
+            _conn = new NpgsqlConnection(Cs);
+            _conn.Open();
+            var cmd = new NpgsqlCommand();
+            cmd.Connection = _conn;
+            cmd.CommandText = "SELECT * FROM CLIENTS;";
+            var dr = cmd.ExecuteReader();
+            if (!dr.HasRows) return;
+            var dt = new DataTable();
+            dt.Load(dr);
+            showClients_pnl.DataSource = dt;
+            
+            _conn.Dispose();
+            _conn.Close();
         }
 
         private void UcClients_Load(object sender, EventArgs e)
@@ -49,82 +60,6 @@ namespace DietApp
 
             // 3) sql select command. Results in showClients_pnl
             
-
-            using (var cmd = new NpgsqlConnection(Cs).CreateCommand())
-            {
-                var sql = "";
-                switch (type)
-                {
-                    case "Name":
-                        sql = "SELECT * FROM clients WHERE fullname = @input;";
-                        cmd.CommandText = sql;
-                        cmd.Parameters.Add("@input", NpgsqlDbType.Char).Value = input;
-                        break;
-                    case "Telephone":
-                        sql = "SELECT * FROM clients WHERE tel = @telephone;";
-                        cmd.CommandText = sql;
-                        cmd.Parameters.Add("@telephone", NpgsqlDbType.Bigint).Value = long.Parse(input);
-                        break;
-                    case "Email":
-                        sql = "SELECT * FROM clients WHERE email = @email;";
-                        cmd.CommandText = sql;
-                        cmd.Parameters.Add("@email", NpgsqlDbType.Char).Value = input;
-                        break;
-                    default:
-                        MessageBox.Show(@"You must select the input type from the dropbox menu!");
-                        break;
-                }
-
-                try
-                {
-                    IDbDataAdapter da = new NpgsqlDataAdapter();
-                    da.SelectCommand = cmd;
-                    var ds = new DataSet();
-                    da.Fill(ds);
-                    var dt = ds.Tables[0];
-                    var i = 0;
-                    foreach (DataRow row in dt.Rows)
-                    {
-                        var lbl = new Label
-                        {
-                            Name = $"link{(row["ID"])}",
-                            Size = new Size(showClients_pnl.Size.Width - 30, 30),
-                            ForeColor = Color.FromArgb(158, 161, 176),
-                            AutoSize = true,
-                            Font = new Font(Font.Name, 11),
-                            TextAlign = ContentAlignment.MiddleLeft,
-                            Text = (i + 1 ) + @" | " + row["fullname"] + @" | " + row["sex"] + @" | " + row["age"] + @" | " + row["height"] + @" | " + row["tel"] + @" | " + row["email"] + @" | " + row["alergies"] + @" | " + row["healthprobs"],
-                            Padding = Padding.Empty,
-                            Margin = new Padding(0, 0, 0, 0)
-                        };
-
-                        var btn = new Button
-                        {
-                            Text = @"Edit",
-                            ForeColor = Color.FromArgb(158, 161, 176),
-                            Font = new Font(Font.Name, 16),
-                            //Size = new Size(50, 30),
-                            AutoSize = true,
-                            Location = new Point(showClients_pnl.Width - 50, 30 * i),
-                            Padding = Padding.Empty,
-                            Margin = new Padding(0, 0, 0, 0),
-                            Cursor = Cursors.Hand,
-                            Visible = true
-                        };
-                        
-                        showClients_pnl.Controls.Add(lbl);
-                        showClients_pnl.Controls.Add(btn);
-                        //btn.Click += (o, args) => { edit_btn_click(row["id"].ToString()); };
-
-                        i++;
-                    }
-                }
-                catch (NpgsqlException exception)
-                {
-                    MessageBox.Show(exception.Message);
-                }
-                
-            }
         }
 
         private static bool IsValidEmail(string email)
@@ -137,6 +72,17 @@ namespace DietApp
             catch
             {
                 return false;
+            }
+        }
+
+        private void showClients_pnl_CellValueChanged(object sender, DataGridViewCellEventArgs e)
+        {
+            // Update client.
+            if (showClients_pnl.CurrentRow == null) return;
+            _conn.Open();
+            using (var cmd = new NpgsqlCommand())
+            {
+                cmd.CommandText = "UPDATE clients SET ";
             }
         }
     }
